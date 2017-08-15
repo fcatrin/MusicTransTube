4f20522839862fe3c48436374ce8a1eff67fd686
Franco Catrin <fcatrin@gmail.com>
2017-08-15 04:41:29 -0300

    Buttons working: Back, Play, Forward, Loop Start and End

--- web/index.php
+++ web/index.php
@@ -12,14 +12,15 @@
 	<div id="video">
 		<div id="videoContainer">
 			<div id="videoDiv">Loading...</div>
+			<div id="loopText"></div>
 		</div>
 	</div>
 	<div id="buttons">
-		<button id="btnBack">Back</button>
+		<button id="btnBack">&lt;&lt;</button>
 		<button id="btnPlay">Play</button>
-		<button id="btnForward">Forward</button>
-		<button id="btnStart">Loop Start</button>
-		<button id="btnEnd">Loop End</button>
+		<button id="btnForward">&gt;&gt;</button>
+		<button id="btnLoopStart">[ LOOP</button>
+		<button id="btnLoopEnd">LOOP ]</button>
 	</div>
 </div>
 </body>
--- web/js/index.js
+++ web/js/index.js
@@ -1,4 +1,7 @@
 var player;
+var loopStart = -1;
+var loopEnd   = -1;
+var MOVE_SECONDS = 2;
 
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('videoDiv', {
@@ -13,13 +16,89 @@ function onYouTubeIframeAPIReady() {
 
 function onPlayerReady(event) {
 	var videoId = getQueryString('v');
-	player.loadVideoById(videoId, 0, 'large');
+	player.cueVideoById(videoId, 0, 'large');
+	
+	$('#btnBack').click(onMoveBackPressed);
+	$('#btnPlay').click(onPlayPausePressed);
+	$('#btnForward').click(onMoveForwardPressed);
+
+	$('#btnLoopStart').click(onLoopStartPressed);
+	$('#btnLoopEnd')  .click(onLoopEndPressed);
+	
+	displayLoopInfo();
+	
+	setInterval(updatePlayerInfo, 250);
 }
 
 function onPlayerStateChange(event) {
 }
 
-function playCurrentVideo() {
-	
+
+function updatePlayerInfo() {
+	var position = player.getCurrentTime();
+	console.log("p:" + position + ", s:" + loopStart + ", e:" + loopEnd);
+	if (loopStart >= 0 && loopEnd >= 0) {
+		if (position >= loopEnd) {
+			player.seekTo(loopStart, true);
+		}
+	}
 }
 
+function onLoopStartPressed() {
+	loopStart = loopStart >= 0 ? -1 : player.getCurrentTime();
+	if (loopStart>=0 && loopStart > loopEnd) {
+		loopEnd = -1;
+	}
+	displayLoopInfo();
+}
+
+function onLoopEndPressed() {
+	loopEnd = loopEnd >= 0 ? -1 : player.getCurrentTime();
+	if (loopEnd>=0 && loopEnd < loopStart) {
+		loopStart = -1;
+	}
+	displayLoopInfo();
+}
+
+function onPlayPausePressed() {
+	var state = player.getPlayerState();
+	if (state == 1) {
+		player.pauseVideo();
+	} else {
+		player.playVideo();
+	}
+}
+
+function onMoveBackPressed() {
+	var position = player.getCurrentTime() - MOVE_SECONDS;
+	if (position < 0) position = 0;
+	
+	player.seekTo(position, true);
+}
+
+function onMoveForwardPressed() {
+	var position = player.getCurrentTime() + MOVE_SECONDS;
+	
+	player.seekTo(position, true);
+}
+
+function displayLoopInfo() {
+	var loopText = "Loop [ ";
+	if (loopStart >= 0) {
+		loopText += loopStart;
+	} else {
+		loopText += "N";
+	}
+	
+	loopText += " -> ";
+	
+	if (loopEnd >= 0) {
+		loopText += loopEnd;
+	} else {
+		loopText += "N";
+	}
+
+	loopText += " ]";
+	
+	$('#loopText').text(loopText);
+}
